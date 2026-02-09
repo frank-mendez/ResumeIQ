baf2a5f8f3ee463332a11287c6ca17717d68275c
frank-mendez <frankmendezresources@gmail.com>
2026-02-09 23:26:11 +0800

    docs: remove everything

--- app/routes/_authenticated.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from '@tanstack:react-router'
-
-export const Route = createFileRoute('/_authenticated')({
-  beforeLoad: async () => {
-    // Authentication check would go here
-    return { user: null }
-  },
-})
