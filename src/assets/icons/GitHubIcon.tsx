bbb70f0060e865d624a07d2f4e419b244b107bff
frank-mendez <frankmendezresources@gmail.com>
2026-02-13 23:02:10 +0800

    fix: sonarqube issue

--- src/assets/icons/GitHubIcon.tsx
+++ src/assets/icons/GitHubIcon.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-export function GitHubIcon(props: React.SVGProps<SVGSVGElement>) {
+export function GitHubIcon(props: Readonly<React.SVGProps<SVGSVGElement>>) {
   return (
     <svg viewBox="0 0 24 24" {...props}>
       <path
