bd68c067fecc85b3420cf980d42afd5643ce4de2
frank-mendez <frankmendezresources@gmail.com>
2026-02-13 22:23:42 +0800

    fix: workflow

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   test: {
     coverage: {
       provider: "v8",
-      reporter: ["text", "lcov"],
+      reporter: ["text", "lcov", "json", "json-summary"],
       reportsDirectory: "coverage",
       thresholds: {
         lines: 70,
